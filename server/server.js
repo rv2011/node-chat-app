10c10c2c6e1e9aabc80adc60e3ada38081c94309
Raghav Vashisht <raghav2011rv@gmail.com>
2018-03-12 18:42:43 +0530

    Added Geolocation feature

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
@@ -41,6 +41,9 @@ io.on('connection', (socket) => {
   // socket.on('createEmail', (newEmail) => {
   //   console.log('createEmail',newEmail);
   // });
+  socket.on('createLocationMessage',(coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
