21236e65fbb63c01a551c47f6719942ac157a5d8
Raghav Vashisht <raghav2011rv@gmail.com>
2018-03-18 00:45:56 +0530

    Add Users class and updated usersList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,20 +17,39 @@ function scrollToBottom () {
 }
 socket.on('connect', function() {
   console.log('Connected to Server.');
-  socket.on('newMessage',function(message) {
-    var formattedTime = moment(message.createdAt).format('h:mm a');
-    var template = jQuery('#message-template').html();
-    var html = Mustache.render(template,{
-      text: message.text,
-      from: message.from,
-      createdAt: formattedTime
-    });
-    jQuery('#messages').append(html);
-    scrollToBottom();
+  var params = jQuery.deparam(window.location.search);
+
+  socket.emit('join', params, function(err) {
+    if(err){
+      alert(err);
+      window.location.href = '/';
+    }else {
+      console.log('No error');
+    }
   });
 });
 socket.on('disconnect', function() {
   console.log('Disconnected from the Server.');
+});
+
+socket.on('updateUserList', function(users) {
+  var ol = jQuery('<ol></ol>');
+  users.forEach(function(user) {
+    ol.append(jQuery('<li></li>').text(user));
+  });
+  jQuery('#users').html(ol);
+});
+
+socket.on('newMessage',function(message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template,{
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
